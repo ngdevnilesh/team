c43c1694399f60e1f2a4873e2333bc147a852098
Nilesh <nilesh.b@osmosys.co>
2025-01-26 22:44:57 +0530

    Update angular.json

---
+++ dist/browser/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://ngdevnilesh.github.io/team',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
